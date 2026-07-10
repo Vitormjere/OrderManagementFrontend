edba7f0d5284b74b1c2e5e45b738f1aed498fcd8
Vitor Miranda <vitormjeremias@hotmail.com>
2026-07-10 18:58:29 -0300

    Add React Router navigation between Products and Customers pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
+import { Routes, Route, Link, Navigate } from 'react-router-dom'
 import ProductsPage from './pages/ProductsPage'
 import CustomersPage from './pages/CustomersPage'
 
 function App() {
   return (
     <div>
-      <ProductsPage />
-      <CustomersPage />
+      <nav>
+        <Link to="/products">Produtos</Link>
+        {' | '}
+        <Link to="/customers">Clientes</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Navigate to="/products" />} />
+        <Route path="/products" element={<ProductsPage />} />
+        <Route path="/customers" element={<CustomersPage />} />
+      </Routes>
     </div>
   )
 }
